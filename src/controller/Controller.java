d494263af55206d1eeb0b8c8c30ff2effa661eeb
simata98 <wlsrnr905@naver.com>
2021-04-06 00:47:33 +0900

    address click added

---
+++ src/controller/Controller.java
@@ -0,0 +1,70 @@
+package controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import org.jnetpcap.Pcap;
+import org.jnetpcap.PcapIf;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private ListView<String> networkListView;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private Button pickButton;
+
+    ObservableList<String> networkList = FXCollections.observableArrayList();
+
+    private ArrayList<PcapIf> allDevs = null;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        allDevs = new ArrayList<PcapIf>();
+        StringBuilder errbuf = new StringBuilder();
+        int r = Pcap.findAllDevs(allDevs, errbuf);
+        if (r == Pcap.NOT_OK || allDevs.isEmpty()) {
+            textArea.appendText("네트워크장치를 찾을 수 없습니다.\n" + errbuf.toString() + "\n");
+        }
+        textArea.appendText("네트워크 장치를 찾았습니다.\n원하시는 장치를 선택하세요\n");
+        for(PcapIf device : allDevs) {
+            networkList.add(device.getName() + " " +
+                    ((device.getDescription() != null) ? device.getDescription() : "설명 없음" ));
+        networkListView.setItems(networkList);
+        }
+    }
+
+    public void networkPickAction() {
+        if (networkListView.getSelectionModel().getSelectedIndex() < 0) {
+            return;
+        }
+        Main.device = allDevs.get(networkListView.getSelectionModel().getSelectedIndex());
+        networkListView.setDisable(true);
+        pickButton.setDisable(true);
+
+        int snaplen = 64 * 1024;
+        int flags = Pcap.MODE_NON_PROMISCUOUS;
+        int timeout = 1;
+
+        StringBuilder errbuf = new StringBuilder();
+        Main.pcap = Pcap.openLive(Main.device.getName(), snaplen, flags, timeout, errbuf);
+
+        if(Main.pcap == null){
+            textArea.appendText("네트워크 장치를 열 수 없습니다.\n" + errbuf.toString() + "\n");
+            return;
+        }
+        textArea.appendText("장치 선택 : " + Main.device.getName() + "\n");
+        textArea.appendText("네트워크장치를 활성화했습니다.");
+    }
+}
---
+++ src/controller/Main.java
@@ -0,0 +1,44 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import org.jnetpcap.Pcap;
+import org.jnetpcap.PcapIf;
+
+public class Main extends Application {
+    public static Pcap pcap = null;
+    public static PcapIf device = null;
+
+    private Stage primaryStage;
+    private AnchorPane layout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("SCH ARP Spoofing");
+        setLayout();
+    }
+
+    public void setLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("../view/View.fxml"));
+            layout = (AnchorPane) loader.load();
+            Scene scene = new Scene(layout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
